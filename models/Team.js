9bd38be91e31931bd821147c61bcb4322a03da15
LaggySon <56988314+LaggySon@users.noreply.github.com>
2021-09-12 01:00:10 -0400

    added players

--- models/Team.js
+++ models/Team.js
@@ -1,27 +1,42 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 const TeamSchema = mongoose.Schema({
-    _id: {
+  _id: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  sheet: {
+    type: String,
+    required: false,
+  },
+  players: [
+    {
+      name: {
         type: String,
-        required: true
+        required: true,
+      },
+      sr: {
+        type: Number,
+        required: true,
+      },
+      role: {
+        type: String,
+        required: true,
+      },
     },
-    link: {
-        type: String,
-        required: true
-    },
-    sheet: {
-        type: String,
-        required: true
-    },
-    name: {
-        type: String,
-        required: true
-    },
-    tier: {
-        type: String,
-        required: true
-    }
-})
+  ],
+  name: {
+    type: String,
+    required: true,
+  },
+  tier: {
+    type: String,
+    required: true,
+  },
+});
 
-module.exports = mongoose.model('Teams', TeamSchema);
+module.exports = mongoose.model("Teams", TeamSchema);
--- routes/teams.js
+++ routes/teams.js
@@ -1,63 +1,67 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Team = require('../models/Team');
+const Team = require("../models/Team");
 
 //GETS ALL POSTS
-router.get('/', async (req,res) => {
-    try{
-        const teams = await Team.find();
-        res.json(teams);
-    } catch (err) {
-        res.json({message: err});
-    }
-})
+router.get("/", async (req, res) => {
+  try {
+    const teams = await Team.find();
+    res.json(teams);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
 
 //SUBMITS A POST
-router.post('/', async (req,res) => {
-    const post = new Team({
-        name: req.body.name,
-        link: req.body.link,
-        tier: req.body.tier,
-        sheet: req.body.sheet,
-        _id: req.body._id
-    });
+router.post("/", async (req, res) => {
+  const post = new Team({
+    name: req.body.name,
+    link: req.body.link,
+    tier: req.body.tier,
+    sheet: req.body.sheet,
+    players: req.body.players,
+    _id: req.body._id,
+  });
 
-    try{
-    const savedPost = await post.save()
+  try {
+    const savedPost = await post.save();
     res.json(savedPost);
-    } catch (err) {
-        res.json({message: err});
-    }
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
 //SPECIFIC POST
-router.get('/:teamId', async (req,res) => {
-    try{
+router.get("/:teamId", async (req, res) => {
+  try {
     const team = await Team.findById(req.params.teamId);
     res.json(team);
-    } catch(err) {
-        res.json({message: err});
-    }
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
 //DELETE POST
-router.delete('/:teamId', async (req,res) => {
-    try {
-    const removedTeam = await Team.remove({_id: req.params.teamId});
+router.delete("/:teamId", async (req, res) => {
+  try {
+    const removedTeam = await Team.remove({ _id: req.params.teamId });
     res.json(removedTeam);
-    } catch (err) {
-        res.json({message: err});
-    }
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
 //UPDATE A POST
-router.patch('/:teamId', async (req,res) => {
-    try {
-    const updatedTeam = await Team.updateOne({_id: req.params.teamId}, {$set: {name: req.body.name}});
+router.patch("/:teamId", async (req, res) => {
+  try {
+    const updatedTeam = await Team.updateOne(
+      { _id: req.params.teamId },
+      { $set: { name: req.body.name } }
+    );
     res.json(updatedTeam);
-    } catch (err) {
-        res.json({message: err});
-    }
-})
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
 
 module.exports = router;
